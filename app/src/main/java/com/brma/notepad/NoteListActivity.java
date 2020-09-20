1aa46f203a4f4dce44f6b6899d93aee6037d02a7
brianmaleek <maleekmalik@gmail.com>
2020-09-20 03:25:08 +0300

    Everything is working fine. Commit entails: 1. Working with RecyclerView and Card View 2. Binding data to RecyclerView 3. RecyclerView and Data Interaction 4. Implementing RecyclerView Adapter 5. Data Management within Recycler Adapter 6. Implementing item selection handling within the app

--- app/src/main/java/com/brma/notepad/NoteListActivity.java
+++ app/src/main/java/com/brma/notepad/NoteListActivity.java
@@ -7,6 +7,8 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 import android.widget.AdapterView;
@@ -16,6 +18,8 @@ import android.widget.ListView;
 import java.util.List;
 
 public class NoteListActivity extends AppCompatActivity {
+
+//    private ArrayAdapter<NoteInfo> mAdapterNotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,29 +41,42 @@ public class NoteListActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+//        mAdapterNotes.notifyDataSetChanged();
+    }
+
     private void initializeDisplayContent() {
-         //Inside the ListView add a list of all the notes. Inorder to do that we need a reference
-        // to the ListView. So we'll declare a variable as below:
-        final ListView listNotes = findViewById(R.id.list_notes);
+//         //Inside the ListView add a list of all the notes. Inorder to do that we need a reference
+//        // to the ListView. So we'll declare a variable as below:
+//        final ListView listNotes = findViewById(R.id.list_notes);
+//
+//         //Will get the list of notes by declaring a variable of notes of type List with a generic
+//        // argument of <NoteInfo>. So we can get those notes and put them in an array adapter
+//        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+//        mAdapterNotes = new ArrayAdapter<>(this,
+//                android.R.layout.simple_list_item_1, notes);
+//        listNotes.setAdapter(mAdapterNotes); //to associate the adapter with the ListView
+//
+//        //anonymous class creation and AS putting the relevant body in it.
+//        listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            //handling list view selection that takes you into NoteActivity
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Intent intent = new Intent(NoteListActivity.this, NoteActivity.class);
+////                NoteInfo note = (NoteInfo) listNotes.getItemAtPosition(position);
+//                intent.putExtra(NoteActivity.NOTE_POSITION, position);
+//                startActivity(intent);
+//            }
+//        });
+        final RecyclerView recyclerNotes = (RecyclerView) findViewById(R.id.list_notes);
+        final LinearLayoutManager notesLayoutManager = new LinearLayoutManager(this);
+        recyclerNotes.setLayoutManager(notesLayoutManager);
 
-         //Will get the list of notes by declaring a variable of notes of type List with a generic
-        // argument of <NoteInfo>. So we can get those notes and put them in an array adapter
-        List<NoteInfo> notes = DataManager.getInstance().getNotes();
-        ArrayAdapter<NoteInfo> adapterNotes = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, notes);
-        listNotes.setAdapter(adapterNotes); //to associate the adapter with the ListView
-
-        //anonymous class creation and AS putting the relevant body in it.
-        listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            //handling list view selection that takes you into NoteActivity
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(NoteListActivity.this, NoteActivity.class);
-//                NoteInfo note = (NoteInfo) listNotes.getItemAtPosition(position);
-                intent.putExtra(NoteActivity.NOTE_POSITION, position);
-                startActivity(intent);
-            }
-        });
+        List <NoteInfo> notes = DataManager.getInstance().getNotes();
+        final NoteRecyclerAdapter noteRecyclerAdapter = new NoteRecyclerAdapter(this, notes);
+        recyclerNotes.setAdapter(noteRecyclerAdapter);
     }
 
 }
